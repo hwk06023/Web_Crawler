30a12c5ebd40f413b9c39923b887811da702bc9e
hwk06023 <hwk06023@gmail.com>
2024-04-30 10:43:31 +0900

    complete Google_news_Entertainment

--- Google_news/Entertainment_selenium.py
+++ Google_news/Entertainment_selenium.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 urls = {
+    "EN": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=en-US&gl=US&ceid=US%3Aen",
+    "CN": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=zh-CN&gl=CN&ceid=CN%3Azh-Hans",
     "KR": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=ko&gl=KR&ceid=KR%3Ako",
     "JP": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=ja&gl=JP&ceid=JP%3Aja",
-    "CN": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=zh-CN&gl=CN&ceid=CN%3Azh-Hans",
-    "EN": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtdHZHZ0pMVWlnQVAB?hl=en-US&gl=US&ceid=US%3Aen",
 }
 
 
@@ -20,33 +20,51 @@ def setup_driver():
     return driver
 
 
-def fetch_entertainment_links(driver, urls):
-    for country, url in urls.items():
-        file_name = f"Entertainment_{country}.txt"
-        with open(file_name, "w", encoding="utf-8") as file:
-            file.write(f"Entertainment News in {country}\n\n")
+def fetch_entertainment_links(country, url):
+    file_name = f"Entertainment_{country}.txt"
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(f"Entertainment News in {country}\n\n")
+        idx = 1
+        while idx < 6:
+            link_tag = None
             driver.get(url)
-            time.sleep(10)
-
-            for i in range(1, 6):
+            time.sleep(5)
+            print(country, idx)
+            crawling_success = False
+            try:
                 link_tag = driver.find_element(
-                    By.CSS_SELECTOR,
-                    f"i10-panel > c-wiz > c-wiz:nth-child({i}) > c-wiz > div > article > a",
+                    By.XPATH,
+                    f'//*[@id="i10-panel"]/c-wiz/c-wiz[{idx}]/c-wiz/div/article/a',
                 )
-                if link_tag:
-                    link_url = link_tag.get_attribute("href")
-                    print(link_url)
-                    driver.get(link_url)
-                    time.sleep(4)
-                    print(driver)
+                crawling_success = True
+            except:
+                link_tag = driver.find_element(
+                    By.XPATH,
+                    f'//*[@id="i10-panel"]/c-wiz/c-wiz[{idx}]/c-wiz/article/div[1]/div[2]/div/a',
+                )
+                crawling_success = True
+            finally:
+                if crawling_success == False:
+                    continue
+            if link_tag:
+                link_url = link_tag.get_attribute("href")
+                driver.get(link_url)
+                time.sleep(6)
 
-                    text = driver.find_element(By.TAG_NAME, "body").text
-                    cleaned_text = " ".join(text.split())
+                text = driver.find_element(By.TAG_NAME, "body").text
+                cleaned_text = " ".join(text.split())
 
-                    file.write(f"{link_url}\n")
-                    file.write(f"{cleaned_text}\n\n")
+                print(link_url)
+                print(cleaned_text)
+
+                file.write(f"{link_url}\n")
+                file.write(f"{cleaned_text}\n\n")
+                idx += 1
 
 
 driver = setup_driver()
-fetch_entertainment_links(driver, urls)
+
+for country, url in urls.items():
+    fetch_entertainment_links(country, url)
+
 driver.quit()
